0a943b580ac33cd9fae19037f1c8269872117247
Ernesto Orellana <ernestoorellana5267@gmail.com>
2020-01-19 09:11:55 -0600

    Test Push

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
         </script>
     </p>
 </footer>
+//Push
 <script type="text/javascript" src="js/jquery-3.4.1.js"></script>
 <script type="text/javascript" src="js/popper.js"></script>
 <script type="text/javascript" src="js/bootstrap.js"></script>
